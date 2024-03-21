f0a6315f9841919d1820b5510107030be0c9121d
折戟沉沙、丿 <36452456+bincooo@users.noreply.github.com>
2024-03-21 22:28:52 +0800

    Patch 1 (#20)
    * fix: 回复引用从CacheSourceQueue中获取
    
    * feat: 发送语音实现
    
    ---------
    
    Co-authored-by: bincooo <bincooo@iMac.local>

--- src/main/java/cn/evole/onebot/mirai/util/VoiceUtils.java
+++ src/main/java/cn/evole/onebot/mirai/util/VoiceUtils.java
@@ -2,12 +2,15 @@ package cn.evole.onebot.mirai.util;
 
 import lombok.val;
 import net.mamoe.mirai.contact.Contact;
+import net.mamoe.mirai.contact.Friend;
 import net.mamoe.mirai.contact.Group;
 import net.mamoe.mirai.message.data.Audio;
+import net.mamoe.mirai.message.data.Message;
 import net.mamoe.mirai.utils.ExternalResource;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.util.Base64;
 
 import static cn.evole.onebot.mirai.OneBotMirai.logger;
 import static cn.evole.onebot.mirai.util.BaseUtils.getDataFile;
@@ -43,5 +46,52 @@ public class VoiceUtils {
             }
             return audio1;
         }, null);
+    }
+
+    public static Message sendRecordMsg(String fileStr, Contact contact) {
+        int index = fileStr.indexOf(":");
+        String protocol;
+        String content;
+        if (index == -1){
+            protocol = "file";
+            content = fileStr;
+        }else{
+            protocol = fileStr.substring(0, index);
+            content = fileStr.substring(index + 3);
+        }
+        switch (protocol) {
+            case "file" -> {
+                File file = new File(content);
+                if (contact instanceof Group group) {
+                    return group.uploadAudio(ExternalResource.create(file));
+                } else if (contact instanceof Friend friend) {
+                    return friend.uploadAudio(ExternalResource.create(file));
+                } else {
+                    throw new IllegalStateException("Unexpected contact: " + contact);
+                }
+            }
+
+            case "http", "https", "base64" -> {
+                byte[] decode;
+                if (protocol.equals("base64")) {
+                    decode = Base64.getDecoder().decode(content);
+                } else {
+                    decode = HttpUtils.getBytesFromHttpUrl(fileStr);
+                }
+
+                if (decode == null || decode.length == 0) {
+                    throw new IllegalStateException("Empty decode");
+                }
+
+                if (contact instanceof Group group) {
+                    return group.uploadAudio(ExternalResource.create(decode));
+                } else if (contact instanceof Friend friend) {
+                    return friend.uploadAudio(ExternalResource.create(decode));
+                } else {
+                    throw new IllegalStateException("Unexpected contact: " + contact);
+                }
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + protocol);
+        }
     }
 }
